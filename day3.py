f8ebe6109a32948faa82a3c559649cd6782b55aa
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-12-04 00:47:45 +0100

    reformat

--- day3.py
+++ day3.py
@@ -1,14 +1,14 @@
 import os
 
-with open(os.path.expanduser('data/day3'), 'r') as f:
+with open(os.path.expanduser("data/day3"), "r") as f:
     lines = f.readlines()
 
 
 n = len(lines)
-n_columns = len(lines[0]) - 1 # no newline character
+n_columns = len(lines[0]) - 1  # no newline character
 gamma = []
 for col in range(n_columns):
-    to_append = int(sum([int(line[col] == '0') for line in lines]) > (n/2))
+    to_append = int(sum([int(line[col] == "0") for line in lines]) > (n / 2))
     gamma.append(to_append)
 
 assert len(gamma) == n_columns
