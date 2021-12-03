1f5966621a44469b5a694cad3a25519540ae94a6
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-12-04 00:42:09 +0100

    initial

---
+++ day3.py
@@ -0,0 +1,20 @@
+import os
+
+with open(os.path.expanduser('data/day3'), 'r') as f:
+    lines = f.readlines()
+
+
+n = len(lines)
+n_columns = len(lines[0]) -1 # no newline character
+gamma = []
+for col in range(n_columns):
+    to_append = int(sum([int(line[col] == '0') for line in lines]) > (n/2))
+    gamma.append(to_append)
+
+assert len(gamma) == n_columns
+
+epsilon = [str(1 - value) for value in gamma]
+epsilon = "".join(epsilon)
+gamma = [str(value) for value in gamma]
+gamma = "".join(gamma)
+int(gamma, 2) * int(epsilon, 2)
