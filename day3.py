b3a7b4f24afbdfac904d15516fc9e68ee5127dea
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-12-04 00:46:33 +0100

    add pre-commit

--- day3.py
+++ day3.py
@@ -5,7 +5,7 @@ with open(os.path.expanduser('data/day3'), 'r') as f:
 
 
 n = len(lines)
-n_columns = len(lines[0]) -1 # no newline character
+n_columns = len(lines[0]) - 1 # no newline character
 gamma = []
 for col in range(n_columns):
     to_append = int(sum([int(line[col] == '0') for line in lines]) > (n/2))
